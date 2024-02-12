d77081af42fa132bbe0174ff646fe9e792742575
track-44 <trackfortyfour@proton.me>
2024-02-13 00:57:37 +1100

    LSP and Clipboard Keymaps

--- lua/track44/defaults.lua
+++ lua/track44/defaults.lua
@@ -1,14 +1,20 @@
+-- Remaps
 -- Leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Remaps
+-- Clipboard
+vim.keymap.set({ "n", "x" }, "<leader>C", '"+y')
+vim.keymap.set({ "n", "x" }, "<leader>P", '"+p')
+
+-- Other
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Plugin/Command Ease of Access
 vim.keymap.set("n", "<leader>fs", vim.cmd.Ex)
 
+-- Options
 -- Line Numbers
 vim.opt.relativenumber = true
 vim.opt.number = true
@@ -22,3 +28,4 @@ vim.opt.smartindent = true
 
 -- Search
 vim.opt.incsearch = true
+
--- lua/track44/plugins/lspzero.lua
+++ lua/track44/plugins/lspzero.lua
@@ -2,34 +2,36 @@ return {
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
-        dependencies = {'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/nvim-cmp', 'L3MON4D3/LuaSnip', 'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim'},
-        config = function ()
+        dependencies = { 'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/nvim-cmp', 'L3MON4D3/LuaSnip', 'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim' },
+        config = function()
             local lsp_zero = require('lsp-zero')
             -- Keymaps when language server is active
             lsp_zero.on_attach(function(client, bufnr)
-                lsp_zero.default_keymaps({buffer=bufnr})
+                lsp_zero.default_keymaps({ buffer = bufnr })
+                vim.keymap.set('n', '<leader>fb', function() vim.lsp.buf.format({ async = false, timeout_ms = 10000 }) end)
+                vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end)
             end)
 
             -- LSP Configuration
             require('mason').setup({})
             require('mason-lspconfig').setup({
-                -- Default Installed LSP 
+                -- Default Installed LSP
                 -- LSP list https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
                 ensure_installed = { 'lua_ls', 'pylsp', 'texlab' },
-                -- Configure LSP 
+                -- Configure LSP
                 handlers = {
                     lsp_zero.default_setup,
                 },
             })
         end
     },
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
     {
         'williamboman/mason.nvim',
         lazy = false
     },
-    {'williamboman/mason-lspconfig.nvim'},
+    { 'williamboman/mason-lspconfig.nvim' },
 }
