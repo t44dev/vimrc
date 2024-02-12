89fd3fcb7c8377d9b9240edce65e4514cc294e6d
track-44 <trackfortyfour@proton.me>
2024-02-13 00:38:46 +1100

    Basic configuration for harpoon and telescope.

--- lua/track44/plugins/telescope.lua
+++ lua/track44/plugins/telescope.lua
@@ -1,4 +1,11 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()        
+        -- Keymaps
+        -- https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#pickers
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>af', builtin.find_files)
+        vim.keymap.set('n', '<leader>gf', builtin.git_files)
+    end
 }
