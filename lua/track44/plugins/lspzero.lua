9501561b44f97d4113c0abc54a8d37266d9722cf
t44dev <trackfortyfour@proton.me>
2024-02-22 09:50:55 +1100

    tsserver default install and live_grep mapping.

--- lua/track44/plugins/lspzero.lua
+++ lua/track44/plugins/lspzero.lua
@@ -22,6 +22,7 @@ return {
                 ensure_installed = {
                     "lua_ls",
                     "pyright",
+                    "tsserver",
                     "texlab"
                 },
                 -- Configure LSP
--- lua/track44/plugins/telescope.lua
+++ lua/track44/plugins/telescope.lua
@@ -6,7 +6,8 @@ return {
         -- Keymaps
         -- https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#pickers
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>af', builtin.find_files)
-        vim.keymap.set('n', '<leader>gf', builtin.git_files)
+        vim.keymap.set('n', '<leader>af', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
     end
 }
